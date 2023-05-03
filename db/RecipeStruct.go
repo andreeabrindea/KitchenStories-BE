1dbfdc07ff5b01bfd4154dcbc66ac70dd6c12ea2
abrindea <andreeabrindea61@gmail.com>
2023-05-03 20:33:04 +0300

    modified recipe type

--- db/RecipeStruct.go
+++ db/RecipeStruct.go
@@ -4,7 +4,7 @@ type Recipe struct {
 	ID           int    `json:"id"`
 	Name         string `json:"name"`
 	UserID       string `json:"user_name"`
-	Photo        string `json:"photo,omitempty"`
+	Photo        []byte `json:"photo,omitempty"`
 	Ingredients  string `json:"ingredients"`
 	Instructions string `json:"instructions"`
 }
